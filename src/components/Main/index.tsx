cf2ace67ad6d4f6505d9aaa99170c3b2bd086f91
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2022-05-13 20:36:41 +0200

    fix main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,11 @@
 import { Button } from "@mui/material"
-import { useContext } from "react"
 import { Header } from "../Header"
-import { UserContext } from "../../contexts/UserContext"
 
 interface MainProps {
   onLogout: () => void;
 }
 
 export const Main: React.FC<MainProps> = ({ onLogout }) => {
-  const currentUser = useContext(UserContext)
-  const navigate = useNavigate()
-
-  const handleLogout = () => {
-    currentUser.isLoggedIn = false;
-    navigate('/login')
-  }
   return (
     <>
       <Header currentContest="Dance Weekend in Warsaw 2023" judge="Leandro Ferreyra" currentCategory="Break until 15:00" />
